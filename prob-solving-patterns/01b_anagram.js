f8a79ea3288acb09c115f0a6209fe4c6b6481adf
Jasmine <jsjohn026@gmail.com>
2020-07-31 19:20:14 -0500

    start anagram exercise

--- prob-solving-patterns/01b_anagram.js
+++ prob-solving-patterns/01b_anagram.js
@@ -1,8 +1,18 @@
 // Anagrams
 // Given two strings, write a function to determine if the
-//  second string is an anagram of the first. An anagram 
+// second string is an anagram of the first. An anagram 
 // is a word, phrase, or name formed by rearranging the 
 // letters of another, such as cinema, formed from iceman
+
+function validAnagram(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  
+  
+}
+
 
 console.log(validAnagram('','')) // true
 console.log(validAnagram('aaz','zza')) // false
@@ -11,8 +21,6 @@ console.log(validAnagram('rat','car')) // false
 console.log(validAnagram('awesome','awesom')) // false
 console.log(validAnagram('qwerty','qeywrt')) // true
 console.log(validAnagram('texttwisttime','timetwisttext')) // true
-
-
 
 
 
