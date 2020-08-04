1476a9c35cd4af30b71044c082fb61fc875cf8f4
Jasmine <jsjohn026@gmail.com>
2020-08-03 19:41:04 -0500

    initial solution for anagram

--- prob-solving-patterns/01b_anagram.js
+++ prob-solving-patterns/01b_anagram.js
@@ -8,9 +8,31 @@ function validAnagram(str1, str2) {
   if (str1.length !== str2.length) {
     return false;
   }
+  
+  const arr1 = str1.split('');
+  const arr2 = str2.split('');
 
+  let counter1 = {};
+  let counter2 = {};
+
+  for (let val of arr1) {
+    counter1[val] = (counter1[val] || 0) + 1;
+  }
+
+  for (let val of arr2) {
+    counter2[val] = (counter2[val] || 0) + 1;
+  }
+
+  for (let key in counter1) {
+    if (!(key in counter2)) {
+      return false;
+    }
+    if ((counter1[key] !== counter2[key])) {
+      return false;
+    }
+  }
   
-  
+  return true;
 }
 
 
