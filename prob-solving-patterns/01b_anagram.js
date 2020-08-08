904c80a6dd06873feeabf0fe454817d525293b68
Jasmine <jsjohn026@gmail.com>
2020-08-08 17:27:26 -0500

    additional anagram solution

--- prob-solving-patterns/01b_anagram.js
+++ prob-solving-patterns/01b_anagram.js
@@ -4,34 +4,59 @@
 // is a word, phrase, or name formed by rearranging the 
 // letters of another, such as cinema, formed from iceman
 
+// function validAnagram(str1, str2) {
+//   if (str1.length !== str2.length) {
+//     return false;
+//   }
+
+//   let counter1 = {};
+//   let counter2 = {};
+
+//   for (let val of str1) {
+//     counter1[val] = (counter1[val] || 0) + 1;
+//   }
+
+//   for (let val of str2) {
+//     counter2[val] = (counter2[val] || 0) + 1;
+//   }
+
+//   for (let key in counter1) {
+//     if (!(key in counter2)) {
+//       return false;
+//     }
+//     if ((counter1[key] !== counter2[key])) {
+//       return false;
+//     }
+//   }
+  
+//   return true;
+// }
+
 function validAnagram(str1, str2) {
   if (str1.length !== str2.length) {
     return false;
   }
 
-  let counter1 = {};
-  let counter2 = {};
+  const lookup = {};
 
-  for (let val of str1) {
-    counter1[val] = (counter1[val] || 0) + 1;
+  for (let i = 0; i < str1.length; i++) {
+    let letter = str1[i];
+    // if letter exists, increment, otherwise set to 1
+    lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
   }
 
-  for (let val of str2) {
-    counter2[val] = (counter2[val] || 0) + 1;
-  }
-
-  for (let key in counter1) {
-    if (!(key in counter2)) {
+  for (let i = 0; i < str2.length; i++) {
+    let letter = str2[i];
+    // can't find letter or letter is zero then it's not an anagram
+    if (!lookup[letter]) {
       return false;
-    }
-    if ((counter1[key] !== counter2[key])) {
-      return false;
+    } else {
+      lookup[letter] -= 1;
     }
   }
-  
+
   return true;
 }
-
 
 console.log(validAnagram('','')) // true
 console.log(validAnagram('aaz','zza')) // false
